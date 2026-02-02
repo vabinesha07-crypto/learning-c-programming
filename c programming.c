ff5a0b14fdaee57e843f5b6261279c879062eff3
abinesha <vabinesha07@gmail.com>
2026-02-02 13:00:43 +0530

    Day 1 completed

--- c programming.c
+++
@@ -1,8 +0,0 @@
-#include <studio.h>
-int main() {
-    int v1=3;
-    int v2=4;
-    int sum=v1+v2;
-    printf("%d",sum);
-    retrun 0;
-}
--- python.py
+++
@@ -1,4 +0,0 @@
-v1=int(input())
-v2=int(input())
-sum=v1+v2
-print("SUM =",sum)
