2bb752587045967451e96b7c7d92fbc5768898a5
abinesha <vabinesha07@gmail.com>
2026-02-04 11:35:13 +0530

    day 3 program

---
+++ jan-feb program in c/4.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int v1=10;
+    int v2=5;
+    int sub;
+    sub=v1-v2;
+    printf("Substraction=%d",sub);
+    return 0;
+}
---
+++ jan-feb program in python/4.2.py
@@ -0,0 +1,4 @@
+v1=int(input())
+v2=int(input())
+sub=v1-v2
+print("Substraction: ",sub )
