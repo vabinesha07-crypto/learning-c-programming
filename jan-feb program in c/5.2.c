33ecd82ba84d132557cfe864ebad16cd8d77a067
abinesha <vabinesha07@gmail.com>
2026-02-05 14:02:32 +0530

    day 4 completed

---
+++ jan-feb program in c/5.2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1,num2,product;
+    printf("Enter two numbers:");
+    scanf("%d %d",&num1,&num2);
+    product=num1*num2;
+    printf("Product=%d\n",product);
+    return 0;
+}
---
+++ jan-feb program in python/5.2.py
@@ -0,0 +1,4 @@
+v1=int(input())
+v2=int(input())
+multiplication=v1*v2
+print("Multiplication:",multiplication)
